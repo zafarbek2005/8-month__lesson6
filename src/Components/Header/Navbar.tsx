7ce78662c4a385a518254d76c20f57cd2176b19e
zafarbek2005 <153996622+zafarbek2005@users.noreply.github.com>
2024-06-15 22:30:41 +0500

    1

---
+++ src/Components/Header/Navbar.tsx
@@ -0,0 +1,74 @@
+import { AiOutlineShoppingCart,  AiOutlineMenu, AiOutlineClose, AiOutlineInstagram } from "react-icons/ai"; 
+import { BsFacebook } from "react-icons/bs"; 
+import React, { useState, useEffect } from 'react';
+import './Navbar.scss';
+import Logo from './logo.svg';
+import { CiHeart } from "react-icons/ci";
+
+const Navbar: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [shrink, setShrink] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setShrink(true);
+    } else {
+      setShrink(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+<>
+
+<header className={shrink ? 'shrink' : ''}>
+      <div className="Navbar container">
+        <div className="logo">
+          <img src={Logo} alt="Logo.svg" />
+        </div>
+
+        <div className={`items ${menuOpen ? 'open' : ''}`}>
+          <a href="#">ALL PRODUCTS</a>
+          <a href="#">ABOUT SEEDRA</a>
+          <a href="#">OUR BLOG</a>
+          <a href="#">CONTACTS</a>
+          <span>
+            <AiOutlineInstagram /> <BsFacebook />
+          </span>
+          <input type="search" placeholder="Search..." />
+        </div>
+
+        <div className="icons">
+          <span><CiHeart /></span>
+          <span><AiOutlineShoppingCart /></span>
+        </div>
+
+        <div className="menu-toggle" onClick={toggleMenu}>
+          {menuOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </div>
+      </div>
+    </header>
+
+
+
+
+
+
+
+
+
+</>
+  );
+};
+
+export default Navbar;
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import './home.scss'
+import opa from './Ellipse 2.svg'
+import aylana from './Ellipse 3.svg'
+import opa2 from './story.svg'
+
+const Home = () => {
+  return (
+    <>
+    <div className="hero container">
+        <h3>Who we are and what we do</h3>
+        <p>Here you can find a lot of interesting and useful information that you need in gardening and creating a beautiful garden</p>
+    </div>
+
+    <div className="text__img container">
+        <div className="txt">
+            <h3>Seedra helps to grow fast and efficiant</h3>
+            <p>SEEDRA Spinach Seeds - contains 600 seeds in 2 Packs and     professional instructions created by PhD Helga George <br /> <br />
+
+                Be sure of our quality - the freshest batches of this season. Non-GMO, Heirloom - our seeds were tested and have the best germination ratings. 
+                Your easy growing experience is our guarantee
+                Spinach commom culinary uses: salads, soups, smoothies, lasagne, pizza, pies, risotto, and more <br /> <br />
+
+                Proudly sourced in the USA - our garden seeds are grown, harvested, and packaged in the USA. We support local farmers and are happy to produce this American-made product
+  </p>
+        </div>
+
+            <div className="img">
+                {/* <img src= {aylana} alt="aylana" /> */}
+                <img src= {opa} alt="opa" />
+            </div>
+    </div>
+
+    <div className="ourstory container">
+
+        <div className="img ">
+            <img src= {opa2} alt="opa2" />
+        </div>
+
+        <div className="txt">
+            <h3>Our story</h3>
+            <p>SEEDRA Spinach Seeds - contains 600 seeds in 2 Packs and professional instructions created by PhD Helga George <br /> <br />
+
+Be sure of our quality - the freshest batches of this season. Non-GMO, Heirloom - our seeds were tested and have the best germination ratings. <br /> 
+Your easy growing experience is our guarantee
+Spinach commom culinary uses: salads, soups, smoothies, lasagne, pizza, pies, risotto, and more <br /> <br />
+
+Proudly sourced in the USA - our garden seeds are grown, harvested, and packaged in the USA. We support local farmers and are happy to produce this American-made product</p>
+        </div>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Home
